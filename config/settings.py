c0bc4488d20d4fecaf7faefb688d9a3e61e72cbd
anggaj021 <anggaj021>
2025-03-13 07:40:50 +0000

    update

--- config/settings.py
+++ config/settings.py
@@ -2,4 +2,4 @@ BRONZE_TABLE = "bronze_sales"
 SILVER_TABLE = "silver_sales"
 GOLD_TABLE = "gold_sales"
 
-LOCAL_FILE_PATH = "/Volumes/mitrais_databricks/angga_test/raw_data/"
+RAW_DATA_PATH = "/Volumes/mitrais_databricks/angga_test/raw_data/"
